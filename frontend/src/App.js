8766a7630fbdaa9d4292fcce37aaa98557727954
sasitha <sasitha@thinkcube.com>
2021-07-25 11:47:27 +0530

    init create react app and added back componments

---
+++ frontend/src/App.js
@@ -0,0 +1,25 @@
+import React, { useState, useCallback } from 'react';
+
+
+import Users from './user/pages/Users';
+// import NewPlace from './places/pages/NewPlace';
+// import UserPlaces from './places/pages/UserPlaces';
+// import UpdatePlace from './places/pages/UpdatePlace';
+// import Auth from './user/pages/Auth';
+// import MainNavigation from './shared/components/Navigation/MainNavigation';
+// import { AuthContext } from './shared/context/auth-context';
+
+const App = () => {
+
+  return (
+      // <Router>
+      //   <MainNavigation />
+      //   <main>{routes}</main>
+      // </Router>
+
+      <Users />
+  
+  );
+};
+
+export default App;
---
+++ frontend/src/user/components/UsersList.js
@@ -0,0 +1,34 @@
+import React from 'react';
+
+import Item from './Item';
+import Card from '../../shared/components/UIElements/Card';
+import './UsersList.css';
+
+const UsersList = props => {
+  //alert("sasas");
+  if (props.items.length === 0) {
+    return (
+      <div className="center">
+        <Card>
+          <h2>No users found.</h2>
+        </Card>
+      </div>
+    );
+  }
+
+  return (
+    <ul className="users-list">
+      {props.items.map(user => (
+        <Item
+          key={user.id}
+          id={user.id}
+          image={user.image}
+          name={user.name}
+          placeCount={user.places.length}
+        />
+      ))}
+    </ul>
+  );
+};
+
+export default UsersList;
